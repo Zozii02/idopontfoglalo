926c987d61f4e808934448012fd22d90afd53867
Zozii02 <kovacs.zoltan1998@gmail.com>
2026-04-04 22:25:31 +0200

    build javítás

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-// Itt változtattuk meg a változó nevét a pontos egyezés kedvéért:
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!
+// Ha a Vercel valamiért nem találja a kulcsot építéskor, berakunk egy "vak" kulcsot, 
+// hogy ne omoljon össze a rendszer, és az építés le tudjon futni!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://ideiglenes.supabase.co"
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "ideiglenes_kulcs_hogy_ne_szalljon_el_a_build"
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
